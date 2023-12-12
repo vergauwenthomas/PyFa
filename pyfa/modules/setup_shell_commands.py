f07e1e0caff056500546f20ebbe5587a5000a4ce
kobebryant432 <kobe.ande@gmail.com>
2023-12-12 17:33:42 +0100

    Future proof

--- pyfa/modules/setup_shell_commands.py
+++ pyfa/modules/setup_shell_commands.py
@@ -7,18 +7,20 @@ Created on Tue Jan 24 14:07:00 2023
 """
 
 import os
+from pathlib import Path
 
-main_path = os.path.dirname(__file__)
-executor_file = os.path.join(main_path, '../../bash_executor.sh')
+executor_directory = Path(__file__).resolve().parents[1]
+file = executor_directory / 'bash_executor.sh'
+print(file)
 
 # Append to Bashrc
 file = os.path.join(os.getenv('HOME'), '.bashrc')
-file1 = open(file, "a")  # append mode
 
 # Only append if not yet appended
 if "# <<<  PyFa  >>>" in open(file, "r").read():
     print('It seems that PyFa is already in you .bashrc')
 else:
+    file1 = open(file, "a")  # append mode
     file1.write("\n")
     file1.write("\n")
     file1.write("# <<<  PyFa  >>> \n")
