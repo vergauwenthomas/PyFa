c3d0a3b6dfb9e90bf637ff59787e260a55574eae
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-12-12 16:00:46 +0100

    fix action on version control

--- pyfa/__init__.py
+++ pyfa/__init__.py
@@ -9,6 +9,7 @@ Created on Mon Jan 23 17:20:08 2023
 # from .lib_functions import setup_shell_command, FA_to_Xarray, get_fields
 
 
-__version__= '0.0.1alpha'
+__version__ = "0.0.1alpha"
+
 
 from .lib_functions import get_fields, FA_to_Xarray, setup_shell_command
